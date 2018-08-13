924343bf3949f4f94db457ba6d0f320b1603e77d
sophie liu <sophieliu@hotmail.fr>
2018-08-13 14:01:33 +0200

    solved the exo

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,18 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-  end
+    @question = params[:question]
+    # raise
+    @variable = "sophie test"
+
+    if @question == "I am going to work"
+      then @variable = "Great!"
+    elsif @question.chars.last == "?"
+      then @variable = "Silly question, get dressed and go to work!."
+    else
+      @variable = "I don't care, get dressed and go to work!"
+    end
+# raise
+end
+
 end
